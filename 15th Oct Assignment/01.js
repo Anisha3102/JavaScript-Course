a390dd621d7bb3b65b9a491c7a131c77ba8dd54c
Anisha3102 <anisha31102002@gmail.com>
2024-10-23 16:07:57 +0530

    Add files via upload

---
+++ 15th Oct Assignment/01.js
@@ -0,0 +1,9 @@
+//1 Create a function greet that takes two positional arguments: name and greeting, and returns a personalized greeting message.
+
+function greet(name,greeting){
+    return name+greeting; ;
+}
+
+let name="Anisha ,";
+let greeting="hii!";
+console.log (greet(name,greeting));
