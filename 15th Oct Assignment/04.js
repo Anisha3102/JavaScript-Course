49fa5ba351522e843602f75c0a212bbdd56f10e6
Anisha3102 <anisha31102002@gmail.com>
2024-10-24 22:25:15 +0530

    Add files via upload

---
+++ 15th Oct Assignment/04.js
@@ -0,0 +1,12 @@
+
+//4 Create a function introduce that takes three positional arguments: firstName, lastName, and age, and returns a sentence introducing the person.
+
+function abc(firstName,lastName,age){
+    
+    return "I am "+firstName+lastName+"of "+age+"years"; 
+}
+
+let firstName="Anisha ";
+let lastName="Das ";
+let age=21;
+console.log (abc(firstName,lastName,age));
