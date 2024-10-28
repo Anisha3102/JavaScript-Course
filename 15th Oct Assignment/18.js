c581cf40ae51362f7c1166682f50823ffb9c88ec
Anisha3102 <anisha31102002@gmail.com>
2024-10-28 11:57:56 +0530

    Add files via upload

---
+++ 15th Oct Assignment/18.js
@@ -0,0 +1,19 @@
+//18 Create an object laptop with both primitive (brand, price) and non-primitive (features - an array) properties.
+
+
+const laptop = {
+    brand: "Dell",
+    price: 999.99,
+    features: [
+        "15.6 inch Full HD display",
+        "Intel Core i7 processor",
+        "16GB RAM",
+        "512GB SSD",
+        "NVIDIA GeForce GTX 1650"
+    ]
+};
+
+
+console.log(laptop.brand);    
+console.log(laptop.price);    
+console.log(laptop.features);  
