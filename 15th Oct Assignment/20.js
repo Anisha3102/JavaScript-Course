77b0f9d444e516448b39ef1ac4dc0f292feadf61
Anisha3102 <anisha31102002@gmail.com>
2024-10-29 22:20:52 +0530

    Add files via upload

---
+++ 15th Oct Assignment/20.js
@@ -0,0 +1,10 @@
+//20 Write a function buildTeam that takes an unknown number of team members using the rest operator and returns an array of their names.
+
+function buildTeam(...nums){
+    const [...a]=nums;
+     return nums;
+  }
+  
+  console.log(buildTeam("Anisha","Uma","Saumyajeet","Priya",));
+  
+  
