1a8943c25794c5ae27461c951a8b8ccda9c63bb8
Anisha3102 <anisha31102002@gmail.com>
2024-11-16 14:15:11 +0530

    Add files via upload

---
+++ 06th Nov Assignment/06.js
@@ -0,0 +1,7 @@
+//6. Use some to check if there is any number greater than 100 in [10, 20, 30, 150].
+
+const arr = [10, 20, 30, 150];
+const hasNumberGreaterThan100 = arr.some(num => num > 100);
+
+console.log(hasNumberGreaterThan100);  
+
