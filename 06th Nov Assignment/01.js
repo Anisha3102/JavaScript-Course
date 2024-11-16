502600b6223d633298fd964648f90090b547ca9d
Anisha3102 <anisha31102002@gmail.com>
2024-11-16 14:13:22 +0530

    Add files via upload

---
+++ 06th Nov Assignment/01.js
@@ -0,0 +1,8 @@
+//1 .Use map to create a new array that doubles the values of the given array [1, 2, 3, 4].
+
+
+const arr = [1, 2, 3, 4];
+const doubledArr = arr.map(num => num * 2);
+
+console.log(doubledArr);  
+
