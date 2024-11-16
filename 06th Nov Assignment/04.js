1ec5a4922a43c78754ec9c3da1a580e766c77813
Anisha3102 <anisha31102002@gmail.com>
2024-11-16 14:14:33 +0530

    Add files via upload

---
+++ 06th Nov Assignment/04.js
@@ -0,0 +1,8 @@
+//4. Use find to get the first element greater than 15 in the array [8, 12, 16, 20, 24].
+
+
+const arr = [8, 12, 16, 20, 24];
+const firstGreaterThan15 = arr.find(num => num > 15);
+
+console.log(firstGreaterThan15);  
+
