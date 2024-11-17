9926dc6d7b79d856008044609b901f80464ffffa
Anisha3102 <anisha31102002@gmail.com>
2024-11-17 20:46:26 +0530

    Add files via upload

---
+++ 06th Nov Assignment/07.js
@@ -0,0 +1,9 @@
+//7.Use every to check if all numbers in the array [5, 10, 15, 20] are multiples of 5.
+
+const arr = [5, 10, 15, 20];
+const allMultiplesOf5 = arr.every(num => num % 5 === 0);
+
+console.log(allMultiplesOf5);  
+
+
+
