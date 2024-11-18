eb13a83aa672fa177bd325794f8b1f0372ab0b92
Anisha3102 <anisha31102002@gmail.com>
2024-11-18 11:46:24 +0530

    Add files via upload

---
+++ 06th Nov Assignment/12.js
@@ -0,0 +1,9 @@
+//12. Use sort to arrange ["banana", "apple", "cherry"] alphabetically.
+
+
+const fruits = ["banana", "apple", "cherry"];
+
+const sortedFruits = fruits.sort();
+
+console.log(sortedFruits);
+
