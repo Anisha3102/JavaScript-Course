685b52978c0ec20f66a49aa48d56faff71b95bdc
Anisha3102 <anisha31102002@gmail.com>
2024-11-20 11:39:19 +0530

    Add files via upload

---
+++ 06th Nov Assignment/19.js
@@ -0,0 +1,6 @@
+//19. Use sort to sort the numbers [50, 20, 10, 40, 30] in descending order.
+
+
+const numbers = [50, 20, 10, 40, 30];
+const sortedNumbers = numbers.sort((a, b) => b - a);
+console.log(sortedNumbers);
