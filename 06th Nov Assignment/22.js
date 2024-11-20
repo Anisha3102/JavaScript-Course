3787f018ba528201d324c78e6c620cf714937ac3
Anisha3102 <anisha31102002@gmail.com>
2024-11-20 20:37:27 +0530

    Add files via upload

---
+++ 06th Nov Assignment/22.js
@@ -0,0 +1,6 @@
+//22. Use filter to get all elements less than 100 from [150, 80, 90, 300].
+
+
+const numbers = [150, 80, 90, 300];
+const lessThan100 = numbers.filter(num => num < 100);
+console.log(lessThan100);
