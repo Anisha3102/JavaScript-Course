e244ee88e4d1d8344a24a67fd8ba95255ca84c7d
Anisha3102 <anisha31102002@gmail.com>
2024-11-20 20:37:04 +0530

    Add files via upload

---
+++ 06th Nov Assignment/21.js
@@ -0,0 +1,8 @@
+
+//21. Use map to transform ["John", "Doe"] into ["john", "doe"] (lowercase).
+
+
+const names = ["John", "Doe"];
+const lowercaseNames = names.map(name => name.toLowerCase());
+console.log(lowercaseNames);
+
