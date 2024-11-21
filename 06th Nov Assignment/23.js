fae481edd20db8fd06a54ab89709292fa1bc468f
Anisha3102 <anisha31102002@gmail.com>
2024-11-21 11:30:01 +0530

    Add files via upload

---
+++ 06th Nov Assignment/23.js
@@ -0,0 +1,7 @@
+
+//23. Use findIndex to locate the position of "cat" in ["dog", "cat", "bird"].
+
+
+const animals = ["dog", "cat", "bird"];
+const index = animals.findIndex(animal => animal === "cat");
+console.log(index);
