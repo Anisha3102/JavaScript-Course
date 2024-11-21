e78f0fc302aabe8430e4081754ccf2704eab48fb
Anisha3102 <anisha31102002@gmail.com>
2024-11-21 11:30:24 +0530

    Add files via upload

---
+++ 06th Nov Assignment/25.js
@@ -0,0 +1,7 @@
+//25. Use some to determine if any number in [25, 35, 45] is greater than 40.
+
+
+const numbers = [25, 35, 45];
+const anyGreaterThan40 = numbers.some(num => num > 40);
+console.log(anyGreaterThan40);
+
