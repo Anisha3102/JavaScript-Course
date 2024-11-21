7e3c1625532dd622e83634f526c52f7612f89d94
Anisha3102 <anisha31102002@gmail.com>
2024-11-21 11:31:04 +0530

    Add files via upload

---
+++ 06th Nov Assignment/27.js
@@ -0,0 +1,7 @@
+
+//27. Use map to append the word "Item" to each element of ["One", "Two", "Three"].
+
+
+const words = ["One", "Two", "Three"];
+const items = words.map(word => word + " Item");
+console.log(items);
