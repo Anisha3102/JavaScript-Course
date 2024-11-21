92cf61801f0eb98bc3138427829c37e9a6a060b0
Anisha3102 <anisha31102002@gmail.com>
2024-11-21 11:29:40 +0530

    Add files via upload

---
+++ 06th Nov Assignment/24.js
@@ -0,0 +1,7 @@
+//24. Use every to confirm if all elements in [10, 20, 30, 40] are greater than 5.
+
+
+const numbers = [10, 20, 30, 40];
+const allGreaterThan5 = numbers.every(num => num > 5);
+console.log(allGreaterThan5);
+
