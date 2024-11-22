0202f70dbd9bb4d1363e74c24a9f0239102ed836
Anisha3102 <anisha31102002@gmail.com>
2024-11-22 14:00:10 +0530

    Add files via upload

---
+++ 06th Nov Assignment/30.js
@@ -0,0 +1,6 @@
+
+//30. Use find to get the first number greater than 100 in [50, 120, 90, 150].
+
+const numbers = [50, 120, 90, 150];
+const firstGreaterThan100 = numbers.find(num => num > 100);
+console.log(firstGreaterThan100);
