16e4cb72584e26426453c28d51abfaffc8b95b46
Anisha3102 <anisha31102002@gmail.com>
2024-11-22 13:59:26 +0530

    Add files via upload

---
+++ 06th Nov Assignment/28.js
@@ -0,0 +1,6 @@
+//28. Use sort to sort an array of characters ["b", "a", "c", "e", "d"] alphabetically.
+
+
+const characters = ["b", "a", "c", "e", "d"];
+const sortedCharacters = characters.sort();
+console.log(sortedCharacters);
