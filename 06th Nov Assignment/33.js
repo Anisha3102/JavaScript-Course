74de78a012d409ce6ac4b0b93eb9e6197ca541bc
Anisha3102 <anisha31102002@gmail.com>
2024-11-22 14:01:14 +0530

    Add files via upload

---
+++ 06th Nov Assignment/33.js
@@ -0,0 +1,8 @@
+//33. Use map to generate an array of lengths of each word in ["apple", "banana", "cherry"].
+
+
+const words = ["apple", "banana", "cherry"];
+
+const lengths = words.map(word => word.length);
+
+console.log(lengths); 
