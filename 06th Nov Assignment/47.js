5640ba609f572ddcf8eb2eb5919264629bacc0f2
Anisha3102 <anisha31102002@gmail.com>
2024-11-25 22:57:19 +0530

    Add files via upload

---
+++ 06th Nov Assignment/47.js
@@ -0,0 +1,8 @@
+//47. Use findIndex to get the index of the first number greater than 10 in [2, 8, 11, 15].
+
+
+const numbers = [2, 8, 11, 15];
+
+const index = numbers.findIndex(num => num > 10);
+
+console.log(index); 
