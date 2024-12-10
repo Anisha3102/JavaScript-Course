34eb4273079f7d125b8c7e8f19d685a707ee0111
Anisha3102 <anisha31102002@gmail.com>
2024-12-10 11:24:37 +0530

    Add files via upload

---
+++ 25th Nov Assignment/09.js
@@ -0,0 +1,9 @@
+//Take an object { 0: "a", 1: "b", 2: "c" } and turn it into an array of values.
+// Output: ["a", "b", "c"]
+
+
+const obj = { 0: "a", 1: "b", 2: "c" };
+
+const valuesArray = Object.values(obj);
+
+console.log(valuesArray);
