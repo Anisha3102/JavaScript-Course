ea4c903cd92c018b74123004b26524552abc8308
Anisha3102 <anisha31102002@gmail.com>
2024-12-10 11:23:50 +0530

    Add files via upload

---
+++ 25th Nov Assignment/07.js
@@ -0,0 +1,9 @@
+
+//Transform an object { x: 10, y: 20, z: 30 } into an array of its keys.
+// Output: ["x", "y", "z"]
+
+const obj = { x: 10, y: 20, z: 30 };
+
+const keysArray = Object.keys(obj);
+
+console.log(keysArray);
