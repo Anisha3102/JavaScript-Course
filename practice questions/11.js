d5154fc8e3554afb97417ac5c68e959877ab1fb7
Anisha3102 <anisha31102002@gmail.com>
2025-01-30 23:49:23 +0530

    Add files via upload

---
+++ practice questions/11.js
@@ -0,0 +1,6 @@
+// The function product should return a string representing the product of firstNumber and secondNumber.
+
+const product = (firstNumber, secondNumber) => {
+  
+  return `The product is ${firstNumber * secondNumber}`;
+};
---
+++ practice questions/12.js
@@ -0,0 +1,6 @@
+// The function incrementAndDivide should increment a given number by 1 and then divide the resulting number by 2. It should then return the final result.
+
+const incrementAndDivide = (number) => {
+  number++;
+  return number / 2;
+};
