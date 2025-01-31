f411a3d8a8941dde2c309a490be63908ea94b2b7
Anisha3102 <anisha31102002@gmail.com>
2025-01-31 23:47:47 +0530

    Add files via upload

---
+++ practice questions/13.js
@@ -0,0 +1,9 @@
+// The function checkExamResult returns Pass if the marks obtained is more than or equal to 45, otherwise it returns Fail.
+
+function checkExamResult(marks) {
+  if (marks >= 45) {
+    return "Pass";
+  } else {
+    return "Fail";
+  }
+}
---
+++ practice questions/14.js
@@ -0,0 +1,5 @@
+// The function isDivisible should determine whether the given number is divisible by 6 or not.
+
+// If the number is divisible by 6, return true. Otherwise, return false.
+
+const isDivisible = number => number % 6=== 0 ? true : false ;
